fd253d680b0c12b5756e2d3b2178c85418f15b24
Michiel de Jong <michiel@pondersource.com>
2024-09-09 15:14:33 +0200

    Split out validate function

--- validate.js
+++ validate.js
@@ -22,10 +22,113 @@ const SLOW_DOCUMENT_THRESHOLD = 10 * 1000; // number of milliseconds after which
 const declarationsPath = path.resolve(process.cwd(), config.get('@opentermsarchive/engine.services.declarationsPath'));
 const instancePath = path.resolve(declarationsPath, '../');
 
+// see https://stackoverflow.com/a/57599885/680454
+export async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+export function split(arr, n) {
+  var res = [];
+  while (arr.length) {
+    res.push(arr.splice(0, n));
+  }
+  return res;
+}
+export const delayMS = (t = 200) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(t);
+    }, t);
+  });
+};
+export const throttledPromises = (
+  asyncFunction,
+  items = [],
+  batchSize = 1,
+  delay = 0
+) => {
+  return new Promise(async (resolve, reject) => {
+    const output = [];
+    const batches= split(items, batchSize);
+    await asyncForEach(batches, async (batch) => {
+      const promises = batch.map(asyncFunction).map(p => p.catch(reject));
+      const results = await Promise.all(promises);
+      output.push(...results);
+      await delayMS(delay);
+    });
+    resolve(output);
+  });
+};
+
+async function validate(serviceId, service, servicesTermsTypes) {
+  const schemaOnly = false;
+  const termsTypes = [];
+
+  const filePath = path.join(declarationsPath, `${serviceId}.json`);
+  const historyFilePath = path.join(declarationsPath, `${serviceId}.history.json`);
+
+  const declaration = JSON.parse(await fs.readFile(filePath));
+  try {
+    assertValid(serviceSchema, declaration);
+  } catch (err) {
+    console.log(`FAIL A ${serviceId} schema`);
+    return;
+  }
+  if (!schemaOnly && service) {
+    await Promise.all(service.getTermsTypes()
+      .filter(termsType => {
+        if (!service.terms[termsType]?.latest) { // If this terms type has been deleted and there is only a historical record for it, but no current valid declaration
+          return false;
+        }
+
+        if (servicesTermsTypes[serviceId] && servicesTermsTypes[serviceId].length > 0) {
+          return servicesTermsTypes[serviceId].includes(termsType);
+        }
+
+        if (termsTypes.length > 0) {
+          return termsTypes.includes(termsType);
+        }
+
+        return true;
+      })
+      .map(async type => {
+        const terms = service.getTerms({ type });
+
+        await Promise.all(terms.sourceDocuments.map(async sourceDocument => {
+          let filteredContent;
+          const { location, executeClientScripts } = sourceDocument;
+
+          try {
+
+            ({ content: sourceDocument.content, mimeType: sourceDocument.mimeType } = await fetch({
+              url: location,
+              executeClientScripts,
+              cssSelectors: sourceDocument.cssSelectors,
+              config: config.get('@opentermsarchive/engine.fetcher'),
+            }));
+            if (!sourceDocument.content) {
+              console.log('          [Tests skipped as URL is not fetchable]');
+            }
+
+            filteredContent = await extract(sourceDocument);
+            console.log(`OK ${serviceId} ${type} ${location} ${filteredContent.length} characters`);
+          } catch (err) {
+            if (err instanceof FetchDocumentError) {
+              console.log(`FAIL B ${serviceId} ${type} fetch ${location}`);
+            } else if (err instanceof ExtractDocumentError) {
+              console.log(`FAIL C ${serviceId} ${type} extract ${location}`);
+            } else {
+              console.log(`FAIL D ${serviceId} ${type} other ${location}`);
+            }
+          }
+      }));
+    }));
+  }
+}
+
 async function run (options) {
-  const schemaOnly = options.schemaOnly || false;
   let servicesToValidate = options.services || [];
-  const termsTypes = options.types || [];
   let servicesTermsTypes = {};
 
   const serviceDeclarations = await services.loadWithHistory(servicesToValidate);
@@ -41,68 +144,11 @@ async function run (options) {
   }
 
   await launchHeadlessBrowser();
+  // batched loop over servicesToValidate
+  // const promises = throttledPromises()
   servicesToValidate.forEach(async (serviceId) => {
     const service = serviceDeclarations[serviceId];
-    const filePath = path.join(declarationsPath, `${serviceId}.json`);
-    const historyFilePath = path.join(declarationsPath, `${serviceId}.history.json`);
-  
-    const declaration = JSON.parse(await fs.readFile(filePath));
-    try {
-      assertValid(serviceSchema, declaration);
-    } catch (err) {
-      console.log(`FAIL ${serviceId} schema`, err);
-      return;
-    }
-    if (!schemaOnly && service) {
-      await Promise.all(service.getTermsTypes()
-        .filter(termsType => {
-          if (!service.terms[termsType]?.latest) { // If this terms type has been deleted and there is only a historical record for it, but no current valid declaration
-            return false;
-          }
-
-          if (servicesTermsTypes[serviceId] && servicesTermsTypes[serviceId].length > 0) {
-            return servicesTermsTypes[serviceId].includes(termsType);
-          }
-
-          if (termsTypes.length > 0) {
-            return termsTypes.includes(termsType);
-          }
-
-          return true;
-        })
-        .map(async type => {
-          const terms = service.getTerms({ type });
-
-          await Promise.all(terms.sourceDocuments.map(async sourceDocument => {
-            let filteredContent;
-            const { location, executeClientScripts } = sourceDocument;
-
-            try {
-
-              ({ content: sourceDocument.content, mimeType: sourceDocument.mimeType } = await fetch({
-                url: location,
-                executeClientScripts,
-                cssSelectors: sourceDocument.cssSelectors,
-                config: config.get('@opentermsarchive/engine.fetcher'),
-              }));
-              if (!sourceDocument.content) {
-                console.log('          [Tests skipped as URL is not fetchable]');
-              }
-
-              filteredContent = await extract(sourceDocument);
-              console.log(`OK ${serviceId} ${type} ${location} ${filteredContent.length} characters`);
-            } catch (err) {
-              if (err instanceof FetchDocumentError) {
-                console.log(`FAIL ${serviceId} ${type} fetch ${location}`);
-              } else if (err instanceof ExtractDocumentError) {
-                console.log(`FAIL ${serviceId} ${type} extract ${location}`);
-              } else {
-                console.log(`FAIL ${serviceId} ${type} other ${location}`);
-              }
-            }
-        }));
-      }));
-    }
+    return validate(serviceId, service, servicesTermsTypes);
   });
   await stopHeadlessBrowser();
 }
